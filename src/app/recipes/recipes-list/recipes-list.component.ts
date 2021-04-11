58578fa328b2d543b1826ba0f6c1819a474eb728
Tanja <tanja120a@gmail.com>
2021-04-11 07:55:15 +0200

    Create recipes detail and list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A test recipe',
+    'test description',
+    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+    new Recipe('A test recipe',
+    'test description',
+    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+    new Recipe('A test recipe',
+    'test description',
+    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+  ]
 
   constructor() { }
 
