662fcc70c454cbff1d6b1c62922a0e30b118f4ac
Tanja <tanja120a@gmail.com>
2021-04-13 16:53:57 +0200

    Added Ingredients in the recipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 import { Recipe } from './recipe.model';
 
@@ -9,13 +10,27 @@ export class RecipesService{
   private recipes: Recipe[] =[
     new Recipe('A test recipe 1',
     'test description',
-    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+    'https://www.simplyrecipes.com/thmb/j1WtO-KNzo7D7e3j5skU2CIQGk4=/1800x1200/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2009__09__caesar-salad-horiz-a-1800-4a465eb53456465091e34138675259c2.jpg',
+    [
+      new Ingredient('Green salad', 1),
+      new Ingredient('Meat', 3),
+      new Ingredient('Croutons', 5)
+    ]),
     new Recipe('A test recipe 2',
     'test description',
-    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+    'https://media-cdn.tripadvisor.com/media/photo-s/13/c5/c9/cb/vegetatian-options.jpg',
+    [
+      new Ingredient('French fries', 20),
+      new Ingredient('Meat',1),
+      new Ingredient('Bread', 2)
+    ]),
     new Recipe('A test recipe 3',
     'test description',
-    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg'),
+    'https://theflexiblechef.com/wp-content/uploads/2015/07/4-July-picnic-1050x695.jpg',
+    [
+      new Ingredient('Strawberries', 10),
+      new Ingredient('Chocolate', 1)
+    ]),
   ]
 
   getRecipes():Recipe[] {
